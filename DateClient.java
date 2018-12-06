b61c2f732a90e0d7c13875d80d88796319473fdd
rina98 <44663673+rina98@users.noreply.github.com>
2018-12-06 14:35:41 +0800

    Create DateClient.java

---
+++ DateClient.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.net.*;
+class DateClient
+{
+    public static void main(String args[]) throws Exception
+    {
+        Socket soc=new Socket(192.168.248.133,43454);        
+        BufferedReader in=new BufferedReader(new InputStreamReader(soc.getInputStream()  ));
+        System.out.println(in.readLine());
+    }    
+}
